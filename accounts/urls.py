1eb65480c8cc458ea060409b1d177d7e4f58ccec
Emmanuel Murairi <emurairi18@alastudents.org>
2021-05-30 09:55:42 +0400

    setup registration and login

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,25 +6,9 @@ from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, 
 
 urlpatterns = [
     path('', views.account_redirect, name='accounts'),
+    path('login', views.signin, name='login'),
+    path('register-farm', views.register_farm, name='register-farm'),
+    path('register-investor', views.register_investor, name='register-investor'),
     path('logout/', LogoutView.as_view(
         template_name='registration/logout.html'), name='logout'),
-    path('password_reset/', PasswordResetView.as_view(
-        template_name='registration/password_reset.html'
-    ),
-         name='password_reset'),
-    path('password_reset/done/', PasswordResetDoneView.as_view(
-        template_name='registration/password_reset_success.html'
-    ),
-         name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
-         PasswordResetConfirmView.as_view(
-             template_name='registration/password_reset_set.html'
-         ),
-         name='password_reset_confirm'),
-    path('reset/done/', PasswordResetCompleteView.as_view(
-        template_name='registration/password_reset_completed.html'
-    ),
-         name='password_reset_complete'),
-    path('profile/', views.Profile.as_view(), name='profile'),
-
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 
 from django.views.generic.edit import UpdateView, CreateView
 from django.utils.decorators import method_decorator
+from django.contrib.auth import authenticate, login
 
 from django.contrib.auth.decorators import login_required
 
@@ -13,6 +14,59 @@ from farm.models import *
 def account_redirect(request):
     return redirect('accounts', request.user.id)
 
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if hasattr(user, 'contact_number'): # investor
+                return redirect('investors')
+            else: # isinstance(user, MiningFarm):
+                return redirect('mining_farms')
+
+    return render(request, "login.html")
+
+
+def register_investor(request):
+    if request.method == 'POST':
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        username = request.POST['username']
+        password = request.POST['password']
+        bio = request.POST['bio']
+
+        investor = Investor(first_name=first_name, last_name=last_name, bio=bio, username=username)
+
+        investor.set_password(password)
+        investor.save()
+        return redirect('mining_farms')
+    
+    else:
+        return render(request, "registration_investor.html")
+
+def register_farm(request):
+    if request.method == 'POST':
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        username = request.POST['username']
+        password = request.POST['password']
+        farm_name = request.POST['farm-name']
+        description = request.POST['description']
+
+        farm = MiningFarm(first_name=first_name, last_name=last_name, description=description, farm_name=farm_name, username=username)
+
+        farm.set_password(password)
+        farm.save()
+        login(request, farm)
+        return redirect('investors')
+
+    else:
+        return render(request, "registration_farm.html")
 
 @method_decorator(login_required, name='dispatch')
 class Profile(UpdateView):
