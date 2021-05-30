9ee2612f57153432419be3a61e62b4f215564f95
Emmanuel Murairi <emurairi18@alastudents.org>
2021-05-30 10:02:20 +0400

    finale

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,9 @@ from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, 
 
 urlpatterns = [
     path('', views.account_redirect, name='accounts'),
-    path('login', views.signin, name='login'),
-    path('register-farm', views.register_farm, name='register-farm'),
-    path('register-investor', views.register_investor, name='register-investor'),
+    path('login/', views.signin, name='login'),
+    path('register-farm/', views.register_farm, name='register-farm'),
+    path('register-investor/', views.register_investor, name='register-investor'),
     path('logout/', LogoutView.as_view(
         template_name='registration/logout.html'), name='logout'),
 ]
