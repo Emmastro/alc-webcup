2ff511f06634d8282a552bf5d6416c06b74dac05
Emmanuel Murairi <emurairi18@alastudents.org>
2021-05-30 08:08:31 +0400

    add admin

--- farm/views.py
+++ farm/views.py
@@ -4,6 +4,7 @@ from invest.models import Investor
 
 def edit_details(request):
     # TODO: load a form to edit farm details
+    # Details to edit: Descripion, 
     return render(request, "edit_details.html", context=locals())
 
 
